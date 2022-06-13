f8da9663d062c38aedd1d39c7c16a66903f72d8d
JCJMA1994 <201214026@uns.edu.pe>
2022-06-13 07:51:03 -0500

    Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no

---
+++ main.py
@@ -0,0 +1,8 @@
+#Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no
+edad = int(input("Cual es tu edad: "))
+
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
+
