98a5c0750a31e6037bf63c54c0a0ca1a6125e2e7
JCJMA1994 <201214026@uns.edu.pe>
2022-06-14 09:35:46 -0500

    Módulo de Operaciones basicas

---
+++ operaciones.py
@@ -0,0 +1,11 @@
+def suma(number1, number2):
+    return number1 + number2
+
+def resta(number1, number2):
+    return number1 - number2
+
+def multiplicacion(number1, number2):
+    return number1 * number2
+
+def division(number1, number2):
+    return number1 / number2
